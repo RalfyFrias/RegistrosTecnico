b3241c735175d520bf8b8701cee2decdbf02ebde
ralfy <ralfy@RALFY>
2024-09-04 10:59:56 -0400

    injectando el service

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Components;
 using RegistroTecnicos.DAL;
+using RegistroTecnicos.Services;
 
 namespace RegistroTecnicos
 {
@@ -20,6 +21,7 @@ namespace RegistroTecnicos
             //Agregamos el contexto al builder con el ConStr
             builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
 
+            builder.Services.AddScoped<TecnicoService>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
