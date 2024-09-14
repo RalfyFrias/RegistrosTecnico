a1b88fae584ac1c13f4b930369c2021f5aac9014
ralfy <ralfy@RALFY>
2024-09-14 17:43:41 -0300

    modificando los services

---
+++ Services/ClienteService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services;
+
+    public class ClienteService
+    {
+    private readonly Contexto _contexto;
+
+    public ClienteService(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public async Task<bool> Existe(int ClienteId)
+    {
+        return await _contexto.Cliente.AnyAsync(t => t.ClienteId ==  ClienteId);
+    }
+
+    private async Task<bool> Insertar(Clientes cliente)
+    {
+        _contexto.Cliente.Add(cliente);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Clientes cliente)
+    {
+        _contexto.Cliente.Update(cliente);
+        var modificado = await _contexto.SaveChangesAsync() > 0;
+        _contexto.Entry(cliente).State = EntityState.Detached;
+        return modificado;
+    }
+
+    public async Task<bool> Guardar(Clientes cliente)
+    {
+        if (!await Existe(cliente.ClienteId))
+        {
+            return await Insertar(cliente);
+        }
+        else
+        {
+            return await Modificar(cliente);
+        }
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        var eliminado = await _contexto.Cliente.Where(t => t.ClienteId == id).ExecuteDeleteAsync();
+        return eliminado > 0;
+    }
+
+    public async Task<Clientes?> Buscar(int id)
+    {
+        return await _contexto.Cliente.AsNoTracking().FirstOrDefaultAsync(t => t.ClienteId == id);
+    }
+
+    public async Task<List<Clientes>> Listar(Expression<Func<Clientes, bool>> Criterio)
+    {
+        return await _contexto.Cliente
+        .Where(Criterio)
+        .ToListAsync();
+    }
+}
---
+++ Services/TrabajoService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services
+{
+    public class TrabajoService
+    {
+        private readonly Contexto _contexto;
+
+        public TrabajoService(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<bool> Existe(int trabajoId)
+        {
+            return await _contexto.Trabajos.AnyAsync(t => t.TrabajoId == trabajoId);
+        }
+
+        private async Task<bool> Insertar(Trabajos trabajo)
+        {
+            _contexto.Trabajos.Add(trabajo);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        private async Task<bool> Modificar(Trabajos trabajo)
+        {
+            _contexto.Trabajos.Update(trabajo);
+            var modificado = await _contexto.SaveChangesAsync() > 0;
+            _contexto.Entry(trabajo).State = EntityState.Detached;
+            return modificado;
+        }
+
+        public async Task<bool> Guardar(Trabajos trabajo)
+        {
+            if (!await Existe(trabajo.ClienteId))
+            {
+                return await Insertar(trabajo);
+            }
+            else
+            {
+                return await Modificar(trabajo);
+            }
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var eliminado = await _contexto.Trabajos.Where(t => t.TrabajoId == id).ExecuteDeleteAsync();
+            return eliminado > 0;
+        }
+
+        public async Task<Trabajos?> Buscar(int id)
+        {
+            return await _contexto.Trabajos
+                .AsNoTracking()
+                .Include(t => t.Clientes)
+                .FirstOrDefaultAsync(t => t.TrabajoId == id);
+        }
+
+        public async Task<List<Trabajos>> Listar(Expression<Func<Trabajos, bool>> Criterio)
+        {
+            return await _contexto.Trabajos
+              .AsNoTracking()
+              .Include(t => t.Clientes)
+              .Include(t => t.Tecnicos)
+            .Where(Criterio)
+            .ToListAsync();
+        }
+    }
+}
