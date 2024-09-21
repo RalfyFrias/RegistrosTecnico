3879f6f0c0d4229deeac33565b620ff43f491a94
ralfy <ralfy@RALFY>
2024-09-21 17:47:46 -0300

    agregando y creando el modelo de prioridades

---
+++ Models/Prioridades.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+        [Required(ErrorMessage = "El Campo Descripción es obligatorio")]
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "El Campo Descripción es obligatorio")]
+
+        public string? Tiempo { get; set; }
+    
+    }
+}
