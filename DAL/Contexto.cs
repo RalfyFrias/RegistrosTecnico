dbc198c9d21490c0ee9d0dadcf8277540ad80bec
ralfy <ralfy@RALFY>
2024-10-28 17:50:29 -0300

    modificando el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -14,6 +14,8 @@ public class Contexto : DbContext
     public DbSet<Prioridades> Prioridad { get; set; }
     public DbSet<Articulos> Articulos { get; set; }
     public DbSet<TrabajosDetalle> TrabajosDetalle { get; set; }
+    public DbSet<Cotizaciones> Cotizaciones { get; set; }
+    public DbSet<CotizacionesDetalle> CotizacionesDetalle { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
